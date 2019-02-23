2e7abe5382b4dc0f33c0bed833f451ea33691507
Cesar Ibarra <cfi2103@columbia.edu>
2019-02-23 18:37:52 -0500

    Add flask stuff

--- index.py
+++ index.py
@@ -1,19 +1,29 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 import requests
 
 app = Flask(__name__)
 
-
-
-session = requests.Session()
-
-
+RADIO = "radio"
+SOUND = "sound"
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/<typ>")
+def get_png(typ):
+    # retrieve image from file system
+    path = None
+    if typ == RADIO:
+        print(RADIO)
+    elif typ == SOUND:
+        print(SOUND)
+
+    # send_file(path, mimetype="image/png")
+    return typ
 
 
 if __name__ == "__main__":
