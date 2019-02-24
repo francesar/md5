6770754a553fbb0f23331963c720d532aacb9ec4
Nick Liu <nl2523@columbia.edu>
2019-02-23 21:51:37 -0500

    outline complete

--- static/js/index.js
+++ static/js/index.js
@@ -1,34 +1,11 @@
-function initMap() {
-    let map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.758896, lng: -73.985130},
-        zoom: 100,
-        mapTypeId: 'satellite'
-    });
-}
 
-/**
- * 
- */
 
-class HazardOverlay {
-    constructor(bounds, image, map) {
-        this.bounds = bounds;
-        this.image = image;
-        this.map = map;
+$(document).ready( function() {
 
-        this.div = null;
-        this.setMap(map);
-    }
+});
 
-    function onAdd() {
-
-    }
-
-    function draw() {
-        
-    }
-
-    function onRemove() {
-
-    }
+function draw() {
+    var canvas = $("#canvas");
+    var width = canvas.width;
+    var height = canvas.height;
 }
