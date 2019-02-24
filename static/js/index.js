1e3b285a24ad9d477dfe9f9da9bd75c670fbc072
Nick Liu <nl2523@columbia.edu>
2019-02-23 22:49:45 -0500

    updated ui

--- static/js/index.js
+++ static/js/index.js
@@ -30,33 +30,3 @@ function draw(e) {
         .attr("stroke", 'green');
     current_head = {x: e["x"], y: e["y"]};
 }
-
-
-// var line;
-
-// var vis = d3.select("body").append("svg") 
-//     .attr("width", 600)
-//     .attr("height", 400)
-//     .on("mousedown", mousedown)
-//     .on("mouseup", mouseup);
-
-// function mousedown() {
-//     var m = d3.mouse(this);
-//     line = vis.append("line")
-//         .attr("x1", m[0])
-//         .attr("y1", m[1])
-//         .attr("x2", m[0])
-//         .attr("y2", m[1]);
-    
-//     vis.on("mousemove", mousemove);
-// }
-
-// function mousemove() {
-//     var m = d3.mouse(this);
-//     line.attr("x2", m[0])
-//         .attr("y2", m[1]);
-// }
-
-// function mouseup() {
-//     vis.on("mousemove", null);
-// }
